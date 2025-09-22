6dce3516f14063261469d9d843ca1d57c17a461b
keisuke-2 <isaacrobles0724@gmail.com>
2025-09-22 17:06:48 +0800

    fix the styles

--- webuidraftjs_wdb/tailwind.config.js
+++ webuidraftjs_wdb/tailwind.config.js
@@ -1,10 +1,10 @@
 const config = {
   darkMode: ["class"],
   content: [
-    "./pages*.{js,ts,jsx,tsx,mdx}",
-    "./components*.{js,ts,jsx,tsx,mdx}",
-    "./app*.{js,ts,jsx,tsx,mdx}",
-    "*.{js,ts,jsx,tsx,mdx}"
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
